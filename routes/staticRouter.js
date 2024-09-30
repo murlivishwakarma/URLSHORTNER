deee5f21d82e8e2df336a5aa62e7f720c6973681
Murli Vishwakarma <134859656+murlivishwakarma@users.noreply.github.com>
2024-09-30 16:39:29 +0530

    Add files via upload

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -3,7 +3,11 @@ const URL=require("../models/url");
 const router=express.Router();
 
 router.get('/',async (req,res)=>{
-    const allurls=await URL.find({})
+
+     if(!req.user) return res.redirect('/login');
+
+
+    const allurls=await URL.find({createdBy:req.user._id});
 
     return res.render('home',{
         urls:allurls,
@@ -11,4 +15,13 @@ router.get('/',async (req,res)=>{
 
 
 })
+
+router.get('/signup',(req,res)=>{
+return res.render("signup");
+})
+
+router.get('/login',(req,res)=>{
+    return res.render("login");
+    })
+
 module.exports=router;
